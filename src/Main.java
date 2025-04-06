30d2a6803ed3024e26b939c13694c6eceb6fc4b1
Andrei <needmoregpt@gmail.com>
2025-04-06 16:25:40 +0300

    anus

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    Solution solution = new Solution();
+//    System.out.println(Arrays.toString(solution.twoSum(new int[]{2, 7, 11, 15}, 9)));
+//    solution.isPalindrome(3);
+        System.out.println(solution.isPalindrome(12000021));
+    }
+}
---
+++ src/Solution.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] array = new int[2];
+        int i = 0;
+        int j = 1;
+        while (i < nums.length - 1) {
+            while (j <= nums.length - 1) {
+            int sum = nums[i] + nums[j];
+            if (sum == target) {
+                array[0] = i;
+                array[1] = j;
+                }
+            j++;
+            }
+        i++;
+        j = i + 1;
+        }
+        return array;
+    }
+
+    public boolean isPalindrome(int x) {
+//        System.out.println(Integer.toString(x));
+        String str = String.valueOf(x);
+        int j = 0;
+        boolean bool=false;
+        if (str.length() == 1) {
+             bool = true; }
+        else {
+                 for (int i = 0; i < str.length() / 2; i++) {
+//                     System.out.println(str.charAt(i));
+//                     System.out.println(str.charAt(str.length() - 1 - i));
+                     if (str.charAt(i) != str.charAt(str.length() - 1 - i))
+                     {break;}
+                     j+=1;
+                     }
+
+                 }
+        if (j == str.length()/2)
+            {bool = true;}
+    return bool;
+    }
+}
